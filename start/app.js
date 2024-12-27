d8c023b84a3f95d0d9b0883abbf20e6aaa15dfe1
Bharath Kumar Reddy <bharathkumar787@icloud.com>
2024-12-28 02:09:30 +0530

    form handling, post method, default middlewares

--- start/app.js
+++ start/app.js
@@ -50,15 +50,11 @@ app.set('view engine','ejs');
 // })
 
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
 
-
-app.get('/',(req,res,next)=>{
-    const a = 5;
-    const b=10;
-    
-    console.log(a+b);
-    next()
-},(req,res)=>{
+app.get('/',(req,res)=>{
     // res.send('hello world'
     res.render('index')
 })
@@ -71,5 +67,10 @@ app.get('/profile', (req,res)=> {
     res.send('this is the profile page')
 })
 
+
+app.post('/get-form-data',(req,res)=> {
+    console.log(req.body);
+    res.send('data received');
+})
 app.listen(3000)
 
